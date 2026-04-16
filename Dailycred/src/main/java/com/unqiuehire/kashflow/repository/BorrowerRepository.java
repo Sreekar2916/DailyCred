cdf54ce6a5e2c00c03e6a9950b965eb256b5aca1
varalabhavitha <varalabhavitha@gmail.com>
2026-04-16 10:41:25 +0530

    BORROWER DUPLICATE ADDED FOR AADHAR CARD

--- Dailycred/src/main/java/com/unqiuehire/kashflow/repository/BorrowerRepository.java
+++ Dailycred/src/main/java/com/unqiuehire/kashflow/repository/BorrowerRepository.java
@@ -3,5 +3,11 @@ package com.unqiuehire.kashflow.repository;
 import com.unqiuehire.kashflow.entity.Borrower;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface BorrowerRepository extends JpaRepository<Borrower,Long> {
+    Optional<Borrower> findByAadharCardNumber(String aadharCardNumber);
+
+    // 🔹 Check if PAN already exists
+    Optional<Borrower> findByPanCardNumber(String panCardNumber);
 }
